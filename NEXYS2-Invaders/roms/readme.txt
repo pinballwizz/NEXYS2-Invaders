2092db17d96fcc15165d0d0a86ffa4bdc04f77b3
Pinball Wiz <70185819+pinballwizz@users.noreply.github.com>
2024-08-04 13:29:39 +1000

    Add files via upload

---
+++ NEXYS2-Invaders/roms/readme.txt
@@ -0,0 +1,8 @@
+place rom files in here.
+
+name            size
+INVADERS.E      2048
+INVADERS.F      2048
+INVADERS.G      2048
+INVADERS.H      2048
+
---
+++ NEXYS2-Invaders/scripts/make_invaders_proms.bat
@@ -0,0 +1,6 @@
+make_vhdl_prom ..\roms\INVADERS.E INVADERS_ROM_E.vhd
+make_vhdl_prom ..\roms\INVADERS.F INVADERS_ROM_F.vhd
+make_vhdl_prom ..\roms\INVADERS.G INVADERS_ROM_G.vhd
+make_vhdl_prom ..\roms\INVADERS.H INVADERS_ROM_H.vhd
+
+pause
